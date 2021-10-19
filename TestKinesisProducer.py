88a26985462d3a9c4ef03f3039efaab0f1b244db
BayAreaLa8s <himanshu.bhadra@bayareala8s.com>
2021-10-19 16:03:10 -0700

    Updated AWS region in Kinessis

--- TestKinesisProducer.py
+++ TestKinesisProducer.py
@@ -7,35 +7,15 @@ import uuid
 
 STREAM_NAME = "ecomm_profile_customer_stream"
 
-
-def get_data():
-
-    customerid = uuid.uuid4()
-
-    return {
-        "_id": {
-            "$oid": "6165d525ac6b9a27e0dd1842"
-        },
-        'event_time': datetime.datetime.now().isoformat(),
-        "customer_id": customerid.hex,
-        "customer_fname": "testuser_fname",
-        "customer_lname": "testuser_lname",
-        "customer_email": "testuser_email@bayareala8s.com",
-        "customer_password": "testuser_password",
-        "customer_street": "testuser_street",
-        "customer_city": "testuser_city",
-        "customer_state": "testuser_state",
-        "customer_zip": 11111
-    }
-
 def generate(stream_name, kinesis_client):
+    customer_id = uuid.uuid1().hex
     while True:
-        data = get_data()
+        data = "{\"customer_id\":" + customer_id + "\"}"
         print(data)
         kinesis_client.put_record(
             StreamName=stream_name,
             Data=json.dumps(data),
-            PartitionKey=customerid.hex)
+            PartitionKey=customer_id)
         time.sleep(2)
 
 
